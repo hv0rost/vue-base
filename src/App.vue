2615ec09e7037e22a6838882c3467314b83081a6
hv0rost <matyash.danil@ubtec.ru>
2022-11-10 21:49:45 +0300

    add fetching while scroll

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="app">
         <h1>Post's page</h1>
+        <my-input
+            placeholder="Search"
+            v-model="searchQuery" 
+        />
         <div class="app_buttons">
             <my-button 
+                
                 @click="showDialog"
             >
                 create post
@@ -17,11 +22,27 @@
             <PostForm @create="createPost"/>
         </my-dialog>
     <PostList 
-        :posts="posts"
+        :posts="sortAndSearch"
         @remove="removePost"
         v-if="!isPostsLoading"
     />
     <div v-else>Loading</div>
+    <div ref="observer" class="observe"></div>
+
+
+<!--     <div class="page_wrapper">
+        <div 
+            v-for="pageNum in totalPage"
+            :key="pageNum"
+            class="page"
+            :class="{
+                'current-page' : page === pageNum
+            }"
+            @click="changePage(pageNum)"
+        >
+            {{pageNum}}
+        </div>
+    </div> -->
     </div>
 </template>
 
@@ -44,7 +65,11 @@ export default {
             sortOptions : [
                 {value: 'title', name: 'By name'},
                 {value: 'body', name: 'By description'}
-            ]
+            ],
+            searchQuery : '',
+            page : 1,
+            limit : 10,
+            totalPage : 0,
         }
     },
     methods: {
@@ -58,27 +83,53 @@ export default {
         showDialog() {
             this.dialogVisible = true
         },
-        async fetchPosts() {
+        async loadPosts() {
             try{
-                this.isPostsLoading = true
-                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
-                this.posts = response.data
+                this.page += 1
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                    params : {
+                        _page : this.page,
+                        _limit : this.limit
+                    }
+                })
+                this.totalPage = Math.ceil(response.headers['x-total-count'] / this.limit)
+                this.posts = [...this.posts, ...response.data]
             } catch(e) {
                 console.log(e);
-            } finally {
-                this.isPostsLoading = false
-            }
-        },
+            } 
+        }
+/*         changePage(pageNum) {
+            this.page = pageNum
+        } */
     },
     mounted() {
-        this.fetchPosts()
+        this.loadPosts();
+        const options = {
+            rootMargin: '0px',
+            threshold : 1.0
+        }
+        const callback = (entries, observer) => {
+            if(entries[0].isIntersecting && this.posts< this.totalPage) {
+                this.loadPosts()
+            }
+        }
+        const observer = new IntersectionObserver(callback, options)
+        observer.observe(this.$refs.observer)
+    },
+    computed : {
+        sortedPost() {
+            return [...this.posts].sort((post1, post2) => {
+                return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+            })
+        },
+        sortAndSearch() {
+            return this.sortedPost.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+        }
     },
     watch: {
-        selectedSort(newValue) {
-            this.posts.sort((post1, post2) => {
-                return post1[newValue]?.localeCompare(post2[newValue])
-            })
-        }
+/*         page() {
+            this.fetchPosts()
+        } */
     }
 }
 </script>
@@ -102,4 +153,17 @@ export default {
     margin-top : 15px;
     justify-content: space-between;
 }
+.page_wrapper {
+    display: flex;
+    margin-top: 15px;
+}
+.page{
+    border: 1px solid white;
+    padding: 10px;
+    margin: 5px;
+}
+.current-page {
+    border: 2px solid purple;
+}
+.observe {}
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-show="posts.length > 0">
         <h3 style="margin-top: 10px">List of posts</h3>
-        <post-item 
+        <transition-group name="post-list">
+            <post-item 
             v-for="post in posts" 
             :post="post"
             :key="post.id"
             @remove="$emit('remove', post)"
          />
+        </transition-group>
     </div>
 
     <h2 v-show="posts.length === 0"
@@ -33,9 +35,21 @@ export default {
 </script>
 
 <style scoped>
-
-form {
-    display: flex;
-    flex-direction: column;
+.post-list {
+    display: inline-block;
+    margin-right: 10px;
 }
+.post-list-enter-active,
+.post-list-leave-active {
+    transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+    opacity: 0;
+    transform: translateX(130px);
+}
+.post-list-move {
+    transition: transform 0.6s ease;
+}
+
 </style>
